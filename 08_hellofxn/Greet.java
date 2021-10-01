8d96dce64ef1cbb830bbc874af875d856d61490d
Lawrence <ljoa30@stuy.edu>
2021-09-30 21:04:02 -0400

    init setup

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,15 @@
+
+// Lawrence Joa
+// APCS
+// HW08 -- Refactor Freshie Zero / hello function
+// 2021-09-30
+
+public class Greet {
+        public static void main(String[] args) {
+                System.out.println("No hablo queso");
+                greet("test");
+        }
+        public static void greet(String s){
+                System.out.println(s);
+        }
+}
