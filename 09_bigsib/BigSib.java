232c8e42e0cadb01042650d99fcc60f11b6fbc43
Lawrence <ljoa30@stuy.edu>
2021-10-03 21:54:02 -0400

    init setup

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,5 +1,14 @@
+/*
+LIR - Lawrence Joa and Steve and John, Ivina Wang and sealie, Raven (Ruiwen) Tang and Bernard
+APCS
+HW09 -- What Are BigSibs Good For? / Using BigSib version of Greet
+2021-10-03
+*/
+
+
+
 public class BigSib {
-	static class BigSib(String name){
+	public static void greet(String name){
 		return("Why, hello there, " + name + ". How do you do?");
 	}
 }
--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,3 +1,10 @@
+/*
+LIR - Lawrence Joa and Steve and John, Ivina Wang and sealie, Raven (Ruiwen) Tang and Bernard
+APCS
+HW09 -- What Are BigSibs Good For? / Using BigSib version of Greet
+2021-10-03
+*/
+
 public class Greet {
     public static void main( String[] args ) {
         BigSib.greet("Flim");
