883f42c5beb2ff57e0ccd409a5b282b55b5ffa0f
Lawrence <ljoa30@stuy.edu>
2021-10-04 20:54:45 -0400

    init setup

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,17 @@
+
+/*
+LIR - Lawrence Joa and Steve and John, Ivina Wang and sealie, Raven (Ruiwen) Tang and Bernard
+APCS
+HW09 -- Refactor Big Sib One / Adding System.out.println before a function
+2021-10-04
+*/
+
+
+
+public class Greet {
+    public static void main( String[] args ){
+        System.out.println( BigSib.greet("Crosby") );
+        System.out.println( BigSib.greet("Stills") );
+        System.out.println( BigSib.greet("Nash") );
+    }
+}
