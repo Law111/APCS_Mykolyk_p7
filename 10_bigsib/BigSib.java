52751346c3924cd9de470ebe935ae24d1263eaee
Lawrence <ljoa30@stuy.edu>
2021-10-04 20:57:59 -0400

    init setup

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,12 @@
+/*
+LIR - Lawrence Joa and Steve and John, Ivina Wang and sealie, Raven (Ruiwen) Tang and Bernard
+APCS
+HW10 -- Refactor Big Sib One / Adding System.out.println before a function
+2021-10-04
+*/
+
+public class BigSib {
+	public static void greet(String name){
+		return(("Why, hello there, " + name + ". How do you do?");
+	}
+}
