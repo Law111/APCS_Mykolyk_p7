b94a92737941f5669d6c91bedc0ab8d7f300111c
Lawrence <ljoa30@stuy.edu>
2021-10-06 00:08:34 -0400

    hw 12

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,3 +1,10 @@
+/*
+LJ - Lawrence Joa(Steve and John) and Joshua Gao(Batman)
+APCS
+HW12 -- On Elder Individuality and the Elimination of Radio Fuzz / Creating unique greetings with additional method setHelloMsg
+2021-10-05
+*/
+
 public class BigSib{
 
 	public static String setHelloMsg(String r){
