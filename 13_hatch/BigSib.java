66fbb5df0d11e9e8f033a2ad4a703330a9e51111
Lawrence <ljoa30@stuy.edu>
2021-10-07 00:36:49 -0400

    hw 13

---
+++ 13_hatch/BigSib.java
@@ -0,0 +1,20 @@
+/*
+LJ - Lawrence Joa(Steve and John) and Joshua Gao(Batman)
+APCS
+HW13 -- Where do BigSibs Come From? / Adding a constructor
+2021-10-06
+*/
+
+public class BigSib{
+
+	private String helloMsg;
+
+
+	public BigSib(String r){
+		helloMsg = r;
+	}
+
+	public static String greet(String s){
+		return helloMsg + " " + s;
+	}
+}
