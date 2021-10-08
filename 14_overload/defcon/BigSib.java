f064a872e72da9a23f7151ffa249ccea9962d844
Lawrence <ljoa30@stuy.edu>
2021-10-08 02:25:00 -0400

    hw 14

---
+++ 14_overload/defcon/BigSib.java
@@ -0,0 +1,17 @@
+/*
+LJR - Lawrence Joa(Steve and John), Joshua Gao(Batman), and Ruby Friedman(Tiffany)
+APCS
+HW14 -- Customize Your Creation
+2021-10-07
+*/
+
+
+public class BigSib {
+	String helloMsg;
+	public BigSib(){
+		helloMsg = "Word up";
+	}
+	public String greet(String s){
+		return HelloMsg + " " + str;
+	}
+}
