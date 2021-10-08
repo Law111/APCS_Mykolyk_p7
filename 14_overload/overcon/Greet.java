fb6953ae226c443294e347a110056c68292f9523
Lawrence <ljoa30@stuy.edu>
2021-10-08 02:50:48 -0400

    hw 14

---
+++ 14_overload/overcon/Greet.java
@@ -0,0 +1,25 @@
+/*
+LJR - Lawrence Joa(Steve and John), Joshua Gao(Batman), and Ruby Friedman(Tiffany)
+APCS
+HW14 -- Customize Your Creation
+2021-10-07
+*/
+
+public class Greet{
+	public static void main(String[] args){
+		String greeting;
+		BigSib richard = new BigSib("Word up");
+                BigSib doctor = new BigSib("Salutations");
+                BigSib kong = new BigSib("Hey ya");
+                BigSib son = new BigSib("Sup");
+
+		greeting = richard.greet("freshman");
+                System.out.println(greeting);
+                greeting = doctor.greet("Dr. Spaceman");
+                System.out.println(greeting);
+                greeting = kong.greet("Kong Fooey");
+                System.out.println(greeting);
+                greeting = son.greet("mom");
+                System.out.println(greeting);
+	}
+}
