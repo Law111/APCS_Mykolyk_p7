56e11299de6213d522b370fc04e97da2525f6d3c
Lawrence <ljoa30@stuy.edu>
2021-10-20 21:15:23 -0400

    hw 21

--- 21_Stats/StatsDriver.java
+++ 21_Stats/StatsDriver.java
@@ -7,9 +7,9 @@ public class StatsDriver {
 		System.out.println("Double Max = " + math.max(2.0,3.0));
 		System.out.println("Int geoMean = " + math.geoMean(2,4));
 		System.out.println("Double geoMean = " + math.geoMean(2.0,4.0));
-    System.out.println("Int Max = " + math.max(1,2,3));
-    System.out.println("Double Max = " + math.max(1.0,2.0,3.0));
-    System.out.println("Int geoMean = " + math.geoMean(2,4,6));
-    System.out.println("Double geoMean = " + math.geoMean(2.0,4.0,6.0));
+		System.out.println("Int Max = " + math.max(1,2,3));
+		System.out.println("Double Max = " + math.max(1.0,2.0,3.0));
+		System.out.println("Int geoMean = " + math.geoMean(2,4,6));
+		System.out.println("Double geoMean = " + math.geoMean(2.0,4.0,6.0));
     }
 }
