8882573d613f8ce1881bbc5f821989768921d8fb
Lawrence <ljoa30@stuy.edu>
2021-11-02 20:12:24 -0400

    hw 29

--- 29_cliforeastr/Commafier.java
+++ 29_cliforeastr/Commafier.java
@@ -1,17 +1,29 @@
+/*
+Team Number int x: Orion Roven, Lawrence Joa, May Qiu
+APCS
+HW29 -- Which Way Do You Roll? // Iterative/Recursive Methods of commafyF
+2021-11-1
+time spent: 2.0 hours
+DISCO
+The args in the foreach loop is an array/list of the things we put after java <filename without .java>
+We need to use Integer.parseInt(s) for explicitly calling in the main method, since commafyF and commafyR take ints.
+We need to use Integer.toString(<var>) to convert ints to strings
+QCC
+There wasn't much difference between our iterative and recursive methods
+*/
+
+
 public class Commafier{
   public static String commafyF(int n){
-   int ctr=0;
    String ans="";
    String xyz=Integer.toString(n);
-    for(int i=xyz.length()-1;i>=0;i--){
-      ans=xyz.substring(i,i+1)+ans;
-      ctr++;
-
-      if (xyz.length()!=3 && ctr%3==0){
-        ans=","+ans;
-      }
-    }
-    return ans;
+   int i = xyz.length();
+   while(i>3){
+     ans = "," + xyz.substring(i-3, i) + ans;
+     i = i - 3;
+   }
+   ans = xyz.substring(0,i) + ans;
+   return ans;
   }
 
   public static String commafyR(int n){
@@ -29,13 +41,18 @@ public class Commafier{
   }
 
 
-  public static void main(String [] args){
-    /*System.out.println(commafyF(1000));
+  public static void main(String[] args){
+    /*
+    System.out.println(commafyF(1000));
     System.out.println(commafyF(12872178));
-    System.out.println(commafyF(100));
+    System.out.println(commafyF(111111));
     System.out.println(commafyR(1000));
     System.out.println(commafyR(12872178));
-    System.out.println(commafyR(100));*/
+    System.out.println(commafyR(111111));
+    */
+    for (String s : args) {
+      System.out.println(commafyR(Integer.parseInt(s)));
+    }
   }
 
 }
