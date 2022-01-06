7816e3c1ff773e38b1a93a4f1a5cfd335d2499e7
Lawrence <ljoa30@stuy.edu>
2022-01-05 20:05:02 -0500

    hw 53

--- 53_insertionsort/InsertionSort.java
+++ 53_insertionsort/InsertionSort.java
@@ -1,25 +1,36 @@
-// Clyde "Thluffy" Sinclair
-// APCS pd0
-// HW53 -- implementing insertion sort
-// 2022-01-06r
-// time spent:  hrs
+// Fred Gang: Joseph Othman, Michael Kamela, Lawrence Joa
+// APCS pd07
+// HW52 -- implementing selection sort
+// 2022-01-05w
+// time spent:  0.3hrs
 
 /******************************
  * class InsertionSort -- implements InsertionSort algorithm
  *
  * ALGO:
+ We will run n-1 passes. At the ith pass, take the element in position i (starting
+ at when i = 1) and swap it to the left, continuously doing so until this element is
+ at least as large as the one to its left. Continue this until we have a completely
+ sorted ArrayList.
  *
  * DISCO
+ As with BubbleSort and SelectionSort, this algorithm requires at most n(n-1)/2
+ "checks" to complete the sort.
+ The best case scenario for InsertionSort is when all elements are already sorted,
+ as then the algo only performs n-1 comparisons and no swaps.
+ The worst case scenario is when elements are in reverse order, as this will require
+ a comparison between every possible pair of elements, giving n(n-1)/2 total checks.
  *
  * QCC
  * q0: How many passes to sort n elements?
- * a0:
+ * a0: We need n-1 passes to sort n elements.
  * q1: What do you know after pass p?
- * a1:
+ * a1: The first p elements form a sorted subArrayList.
  * q2: How will you know when sorted?
- * a2:
+ * a2: After the (n-1)-th pass then the ArrayList is certainly sorted.
  * q3: What constitues a pass?
- * a3:
+ * a3: Take the element at index i, then swap it to the left until our element
+ is at least as large as the one to its left.
  * q4: What must you track?
  * a4:
  ******************************/
@@ -111,6 +122,7 @@ public class InsertionSort
 
   public static void main( String [] args )
   {
+      /*===============for VOID methods=============
       System.out.println("\n*** Testing sort-in-place (void) version... *** ");
       ArrayList glen = new ArrayList<Integer>();
       glen.add(7);
@@ -121,12 +133,10 @@ public class InsertionSort
       System.out.println( "\nArrayList glen before sorting:\n" + glen );
       insertionSortV(glen);
       System.out.println( "\nArrayList glen after sorting:\n" + glen );
-
       ArrayList coco = populate( 10, 1, 1000 );
       System.out.println( "\nArrayList coco before sorting:\n" + coco );
       insertionSortV(coco);
       System.out.println( "\nArrayList coco after sorting:\n" + coco );
-      /*===============for VOID methods=============
       ============================================*/
 
       System.out.println( "*** Testing non-void version... *** " );
