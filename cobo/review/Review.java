7d396bb5c5a56a79ea0df1f370c2c4344ced2a2d
Lawrence <ljoa30@stuy.edu>
2022-02-13 21:29:59 -0500

    review

--- cobo/review/Review.java
+++ cobo/review/Review.java
@@ -1,3 +1,11 @@
+/*
+Watermelon (Lea Kwok, Nina Jiang, Lawrence Joa)
+APCS pd7
+L06: Read/Review/Expand
+2022-02-14
+time spent: 1.5 hr
+*/
+
 import java.util.Scanner;
 import java.io.File;
 import java.util.HashMap;
@@ -9,14 +17,14 @@ import java.io.*;
  * Class that contains helper methods for the Review Lab
  **/
 public class Review {
-  
+
   private static HashMap<String, Double> sentiment = new HashMap<String, Double>();
   private static ArrayList<String> posAdjectives = new ArrayList<String>();
   private static ArrayList<String> negAdjectives = new ArrayList<String>();
- 
-  
+
+
   private static final String SPACE = " ";
-  
+
   static{
     try {
       Scanner input = new Scanner(new File("cleanSentiment.csv"));
@@ -30,22 +38,22 @@ public class Review {
     catch(Exception e){
       System.out.println("Error reading or parsing cleanSentiment.csv");
     }
-  
-  
+
+
   //read in the positive adjectives in postiveAdjectives.txt
      try {
       Scanner input = new Scanner(new File("positiveAdjectives.txt"));
       while(input.hasNextLine()){
         String temp = input.nextLine().trim();
-        System.out.println(temp);
+        //System.out.println(temp);
         posAdjectives.add(temp);
       }
       input.close();
     }
     catch(Exception e){
       System.out.println("Error reading or parsing postitiveAdjectives.txt\n" + e);
-    }   
- 
+    }
+
   //read in the negative adjectives in negativeAdjectives.txt
      try {
       Scanner input = new Scanner(new File("negativeAdjectives.txt"));
@@ -56,25 +64,25 @@ public class Review {
     }
     catch(Exception e){
       System.out.println("Error reading or parsing negativeAdjectives.txt");
-    }   
+    }
   }
-  
-  /** 
-   * returns a string containing all of the text in fileName (including punctuation), 
-   * with words separated by a single space 
+
+  /**
+   * returns a string containing all of the text in fileName (including punctuation),
+   * with words separated by a single space
    */
   public static String textToString( String fileName )
-  {  
+  {
     String temp = "";
     try {
       Scanner input = new Scanner(new File(fileName));
-      
+
       //add 'words' in the file to the string, separated by a single space
       while(input.hasNext()){
         temp = temp + input.next() + " ";
       }
       input.close();
-      
+
     }
     catch(Exception e){
       System.out.println("Unable to locate " + fileName);
@@ -82,9 +90,9 @@ public class Review {
     //make sure to remove any additional space that may have been added at the end of the string.
     return temp.trim();
   }
-  
+
   /**
-   * @returns the sentiment value of word as a number between -1 (very negative) to 1 (very positive sentiment) 
+   * @returns the sentiment value of word as a number between -1 (very negative) to 1 (very positive sentiment)
    */
   public static double sentimentVal( String word )
   {
@@ -97,12 +105,113 @@ public class Review {
       return 0;
     }
   }
-  
+
+  public static double totalSentiment(String fileName) {
+    String temp = "";
+    double totalSentiment = 0.0;
+    String test = textToString(fileName);
+    test = removePunctuation(test);
+
+    for(int i = 0; i < test.length(); i++){
+      if(test.substring(i, i + 1).equals(" ")){
+        totalSentiment += sentimentVal(temp);
+        temp = "";
+      } else {
+        temp += test.substring(i, i + 1);
+      }
+    } //end for loop
+    return totalSentiment;
+  } //end totalSentiment
+
+  public static String fakeReview(String fileName) {
+    String review = textToString(fileName);
+    String fake = "";
+
+    for(int i = 0; i < review.length() - 1; i++){
+      if(review.substring(i, i + 1).equals("*")){
+        String temp = "";
+        boolean isWord = true;
+        i++;
+
+        while(isWord){
+          temp += review.substring(i, i + 1);
+          i++;
+
+          if(review.substring(i, i + 1).equals(" ")){
+            isWord = false;
+          }
+
+        } //end while loop
+
+        temp = randomAdjective();
+        temp = removePunctuation(temp);
+        fake += temp + " ";
+      } else {
+        fake += review.substring(i, i + 1);
+      } //end if-else
+
+    } //end for loop
+    return fake;
+  } //end fakeReview
+
+
+  public static String fakeReviewStronger(String fileName) {
+    String review = textToString(fileName);
+    String fake = "";
+
+    for(int i = 0; i < review.length() - 1; i++){
+      if(review.substring(i, i + 1).equals("*")){
+        String temp = "";
+        boolean isWord = true;
+        i++;
+
+        while(isWord){
+          temp += review.substring(i, i + 1);
+          i++;
+
+          if(review.substring(i, i + 1).equals(" ")){
+            isWord = false;
+          } //end if
+
+        } //end while loop
+
+        temp = removePunctuation(temp);
+        if (sentimentVal(temp) > 0) {
+          temp = randomNegativeAdj() + " ";
+        } else if (sentimentVal(temp) < 0) {
+          temp = randomPositiveAdj() + " ";
+        } else {
+          temp = randomAdjective() + " ";
+        } //end if-else
+        fake += temp;
+      } else {
+        fake += review.substring(i, i + 1);
+      } //end first if
+    } //end for loop
+    return fake;
+  } //end fakeReview
+
+
+  public static int starRating(String fileName) {
+    double totalSentiment = totalSentiment(fileName);
+    if(totalSentiment >= 15) {
+      return 4;
+    } else if(totalSentiment >= 10) {
+      return 3;
+    } else if(totalSentiment >= 5) {
+      return 2;
+    } else if(totalSentiment >= 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
   /**
-   * Returns the ending punctuation of a string, or the empty string if there is none 
+   * Returns the ending punctuation of a string, or the empty string if there is none
    */
   public static String getPunctuation( String word )
-  { 
+  {
     String punc = "";
     for(int i=word.length()-1; i >= 0; i--){
       if(!Character.isLetterOrDigit(word.charAt(i))){
@@ -113,7 +222,7 @@ public class Review {
     }
     return punc;
   }
-  
+
     /**
    * Returns the word after removing any beginning or ending punctuation
    */
@@ -127,11 +236,11 @@ public class Review {
     {
       word = word.substring(0, word.length()-1);
     }
-    
+
     return word;
   }
-  
-  /** 
+
+  /**
    * Randomly picks a positive adjective from the positiveAdjectives.txt file and returns it.
    */
   public static String randomPositiveAdj()
@@ -139,18 +248,18 @@ public class Review {
     int index = (int)(Math.random() * posAdjectives.size());
     return posAdjectives.get(index);
   }
-  
-  /** 
+
+  /**
    * Randomly picks a negative adjective from the negativeAdjectives.txt file and returns it.
    */
   public static String randomNegativeAdj()
   {
     int index = (int)(Math.random() * negAdjectives.size());
     return negAdjectives.get(index);
-    
+
   }
-  
-  /** 
+
+  /**
    * Randomly picks a positive or negative adjective and returns it.
    */
   public static String randomAdjective()
@@ -161,5 +270,32 @@ public class Review {
     } else {
       return randomNegativeAdj();
     }
+  }
+
+  public static int fakeStarRating(String fileName) {
+    double totalSentiment = totalSentiment(fileName);
+    if(totalSentiment >= 15) {
+      return 0;
+    } else if(totalSentiment >= 10) {
+      return 1;
+    } else if(totalSentiment >= 5) {
+      return 3;
+    } else if(totalSentiment >= 0) {
+      return 2;
+    } else {
+      return 4;
+    }
+  }
+
+  public static void main(String[] args) {
+    System.out.println(sentimentVal("cold"));
+    System.out.println(totalSentiment("SimpleReview.txt"));
+    System.out.println(totalSentiment("TestReview.txt"));
+
+    System.out.println(fakeReview("fakeReview.txt"));
+    System.out.println(fakeReviewStronger("fakeReview.txt"));
+
+    System.out.println(starRating("SimpleReview.txt"));
+    System.out.println(fakeStarRating("SimpleReview.txt"));
   }
 }
